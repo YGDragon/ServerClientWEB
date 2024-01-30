a375233644585966eef7aef94a1294efc8bfc9c8
Golikov-Andrey <newsc2@yandex.ru>
2024-01-30 23:10:16 +0300

    solition

--- Seminar7/Exml1Sem7/spring_security_demo/src/main/java/com/example/spring_security_demo/controller/HelloController.java
+++ Seminar7/Exml1Sem7/spring_security_demo/src/main/java/com/example/spring_security_demo/controller/HelloController.java
@@ -1,2 +1,13 @@
-package com.example.spring_security_demo.controller;public class HelloController {
+package com.example.spring_security_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
 }
