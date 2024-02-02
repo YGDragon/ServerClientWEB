2c95b3736287f3653fd14ff695015e95e5a1cddf
Golikov-Andrey <newsc2@yandex.ru>
2024-02-03 00:22:48 +0300

    solition

--- Seminar8/Empl3Sem8/empl3sem8/src/main/java/ru/geekbrain/empl3sem8/aspect/LogException.java
+++ Seminar8/Empl3Sem8/empl3sem8/src/main/java/ru/geekbrain/empl3sem8/aspect/LogException.java
@@ -1,4 +1,4 @@
-package org.example.aspects;
+package ru.geekbrain.empl3sem8.aspect;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,5 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface ToLog {
+public @interface LogException {
 }
+
